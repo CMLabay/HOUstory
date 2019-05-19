34ef7738abff0016cf19042cbf1618c7f5826c55
Vernon Williams <vwill035@fiu.edu>
2019-05-19 10:16:12 -0500

    Update login logic.

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 'use strict';
 
-function displaySignup(){
-    $('.submit-form').append(
-        `
-            <input type='text' name='password2' id='password2' placeholder='confirm password'>
-            <button type='submit'>Create Account</button>
-        `
-    );
-}
-function displayLogin(){
-    $('.submit-form').append(
-        `<form class='login-form>
-            <label for 'username'>Username</label>
-            <input type='text' name='username' id='username'>
-            <label for 'password'>Password</label>
-            <input type='text' name='password' id='password'>
-            <button type='submit'>Login!</button>
-        </form>`
-    );
-}
-function handleButtons(){
-    $(".login").on('click', '.login-btn',function(event){
-        $('.submit-form').empty();
-        displayLogin();
-    });
-    $(".login").on('click', '.signup-btn',function(event){
-        $('.submit-form').empty();
-        displaySignup();
-    });
+let loggedIn = { status: false};
 
+function handleButton(){
+  $(".login").on('click', '.login-btn',function(event){
+      loggedIn.status = true;
+      localStorage.setItem("status", loggedIn.status);
+  });
 }
-$(handleButtons());
+
+$(document).ready(handleButton());
